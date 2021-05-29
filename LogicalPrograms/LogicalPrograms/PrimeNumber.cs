cb535ba086efd11d1832fe9d659f2c2250e19dc3
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-05-29 21:52:56 +0530

    [Anjaneya]refactor:Prime Number program

---
+++ LogicalPrograms/LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+   public class PrimeNumber
+    {
+        int n, i, NumberLength = 0, temp = 0;
+        public void PrimeNumberCheck()
+        {
+            
+            Console.Write("Enter the Number to check Prime: ");
+            n = int.Parse(Console.ReadLine());
+            NumberLength = n / 2;
+            for (i = 2; i <= NumberLength; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    temp = 1;
+                }
+            }
+            if (temp == 0)
+                Console.Write("Number is Prime.");
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -7,14 +7,20 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("1:Fibonacci series program");
+            Console.WriteLine("2:PrimeNumber program");
             int choice = Convert.ToInt32(Console.ReadLine());
-
             switch (choice)
             {
                 case 1:
 
                     FibonacciSeries fibonacci = new FibonacciSeries();
                     fibonacci.FibonacciNumber();
+                    break;
+
+                case 2:
+
+                    PrimeNumber primenumber = new PrimeNumber();
+                    primenumber.PrimeNumberCheck();
                     break;
             }
         }
