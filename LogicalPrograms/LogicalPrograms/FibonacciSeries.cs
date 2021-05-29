fbc67b23393013e53f84a5f1703a8b94cef648c2
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-05-29 21:20:43 +0530

    [Anjaneya]refactor:Fibonacci series program

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        int number1 = 0;
+        int number2 = 1;
+        int number3;
+        int i;
+        int numberLength;
+        public void FibonacciNumber()
+        {
+            Console.Write("Enter a number :");
+            numberLength = int.Parse(Console.ReadLine());
+            Console.Write(number1 + " " + number2 + " ");//only printing the value of number1 and number2 here    
+            for (i = 0; i < numberLength; i++)
+            {
+                number3 = number1 + number2;
+                Console.Write(number3 + " ");
+                number1 = number2;
+                number2 = number3;
+
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1:Fibonacci series program");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    fibonacci.FibonacciNumber();
+                    break;
+            }
+        }
+    }
+}
